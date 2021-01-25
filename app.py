4ea72fd47924d6aee8a83df6530fae605f5b6f05
nic-was <44951135+nic-was@users.noreply.github.com>
2021-01-25 18:28:09 +0000

    Adding create_projects.html page to add a new project

--- app.py
+++ app.py
@@ -93,6 +93,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/create_project")
+def create_project():
+    return render_template("create_projects.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
