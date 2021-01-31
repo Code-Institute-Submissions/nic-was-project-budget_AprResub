f71e962a36f1a47ebe2c14006492b5351f3137cf
nic-was <44951135+nic-was@users.noreply.github.com>
2021-01-31 01:24:57 +0000

    Adding js code to be able to add categories dynamically when clicking the add categories button. Amending the create projects template to bring in the additional categories based on the js code

---
+++ static/js/categories.js
@@ -0,0 +1,52 @@
+
+
+$(document).ready(function(){
+    let categoryCount = 1
+    
+    $("#add-category").click(function(){
+        categoryCount++
+
+
+        $("#categories").append(`
+        
+            <div class="row category" id="${categoryCount}category">
+                <div class="input-field col s5 m5">
+                    <input name="${categoryCount}category" type="text" 
+                        minlength="5" maxlength="50"  class="validate">
+                    <label for="${categoryCount}category">Category Name</label>
+                </div>
+                <div class="input-field col s5 m5">
+                    <select name="${categoryCount}category" class="validate">
+                        <option value="per unit" selected>per unit</option>
+                        <option value="per hour">per hour</option>
+                        <option value="per day">per day</option>
+                    </select>   
+                </div>
+                <div class="col s2 m2">
+                    <div class="btn"><i class="fas fa-times"></i>
+                    </div>
+                </div>
+            </div>
+
+        `)
+ 
+        // dropdown
+        $(`select`).formSelect()
+
+        $(`#${categoryCount}category .btn`).click(function(){
+            $(this).closest(".category").remove()
+            
+        })
+    })
+
+
+  });
+
+
+
+  
+
+
+
+
+
